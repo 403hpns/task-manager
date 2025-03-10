40ed3bd7a2aa8805107ebd896ef020884dd1d652
403hpns <dev.403hpns@pm.me>
2025-03-10 16:04:12 +0100

    feat(api): add task schema

--- apps/api/src/tasks/schemas/task.schema.ts
+++ apps/api/src/tasks/schemas/task.schema.ts
@@ -3,16 +3,24 @@ import { HydratedDocument } from 'mongoose';
 
 export type TaskDocument = HydratedDocument<Task>;
 
+type Priority = 'low' | 'medium' | 'high';
+
 @Schema()
 export class Task {
   @Prop()
   title: string;
 
-  @Prop()
-  description: string;
+  @Prop({ nullable: true, default: null })
+  description?: string;
 
-  @Prop()
-  completed: boolean;
+  @Prop({ default: false })
+  completed?: boolean;
+
+  @Prop({ type: String, default: 'low' })
+  priority?: Priority;
+
+  @Prop({ type: Date, nullable: true })
+  dueDate?: Date;
 }
 
 export const TaskSchema = SchemaFactory.createForClass(Task);
